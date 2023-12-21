e6936422cd92a120732780a02a55942f27d8232c
Brinda <brindashree29@gmail.com>
2023-12-21 21:49:27 +0530

    feat: shadcn ui integration w/ themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
- 
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <ThemeProvider attribute="class" defaultTheme="system">
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }

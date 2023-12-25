aa6816824507895a5d9cf9addf7b3a7130076cd2
Brinda <brindashree29@gmail.com>
2023-12-25 09:41:09 +0530

    feat: form preview

--- src/components/PreviewDialogBtn.tsx
+++ src/components/PreviewDialogBtn.tsx
@@ -1,13 +1,40 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { MdPreview } from "react-icons/md";
+import useDesigner from "./hooks/useDesigner";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { FormElements } from "./FormElements";
 
 const PreviewDialogBtn = () => {
+  const { elements } = useDesigner();
+
   return (
-    <Button className="gap-2" variant={"outline"}>
-      <MdPreview className="h-4 w-4" />
-      Preview
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="gap-2" variant={"outline"}>
+          <MdPreview className="h-4 w-4" />
+          Preview
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0">
+        <div className="px-4 py-2 border-b">
+          <p className="text-lg font-bold text-muted-foreground">
+            Form preview
+          </p>
+          <p className="text-sm text-muted-foreground">
+            This is how your form will look like to your users.
+          </p>
+        </div>
+        <div className="bg-accent flex flex-col flex-grow items-center justify-center p-4 bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)] overflow-y-auto">
+          <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background h-full w-full rounded-2xl p-8 overflow-y-auto">
+            {elements.map((el)=>{
+              const FormComponent = FormElements[el.type].formComponent;
+              return <FormComponent key={el.id} elementInstance={el}/>
+            })}
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- src/components/fields/TextField.tsx
+++ src/components/fields/TextField.tsx
@@ -43,7 +43,7 @@ const propertiesSchema = z.object({
 export const TextFieldFormElement: FormElement = {
   type,
   designerComponent: DesignerComponent,
-  formComponent: () => <div>Form component</div>,
+  formComponent: FormComponent,
   propertiesComponent: PropertiesComponent,
   construct: (id: string) => ({
     id,
@@ -74,6 +74,26 @@ function DesignerComponent({
         {required === true && "*"}
       </Label>
       <Input readOnly disabled placeholder={placeholder} />
+      {helperText && (
+        <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+      )}
+    </div>
+  );
+}
+function FormComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, placeholder, helperText } = element.extraAttributes;
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label>
+        {label}
+        {required === true && "*"}
+      </Label>
+      <Input placeholder={placeholder} />
       {helperText && (
         <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
       )}
